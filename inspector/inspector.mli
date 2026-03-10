67615d59d78a642ccb1267c8e4d9b74ceade7914
sarika <sarika@platform9.com>
2026-03-10 18:36:38 +0530

    Update inspector.mli

--- inspector/inspector.mli
+++ inspector/inspector.mli
@@ -15,6 +15,6 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *)
-
+// test
 (* virt-v2v-inspector main program.
    Nothing is exported. *)
